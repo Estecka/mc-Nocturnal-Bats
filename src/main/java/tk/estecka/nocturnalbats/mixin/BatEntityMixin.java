20dcc6ae0a4961f28cda90c41e305115cfdff8ca
Estecka <estecka@galacsys.com>
2026-08-15 19:35:51 +0200

    Port to MC 26

--- src/main/java/tk/estecka/nocturnalbats/mixin/BatEntityMixin.java
+++ src/main/java/tk/estecka/nocturnalbats/mixin/BatEntityMixin.java
@@ -1,22 +1,31 @@
 package tk.estecka.nocturnalbats.mixin;
 
-import net.minecraft.entity.passive.BatEntity;
-import net.minecraft.util.math.BlockPos;
-// import net.minecraft.world.LightType;
-import net.minecraft.world.WorldAccess;
+import net.minecraft.core.BlockPos;
+// import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.ambient.Bat;
+import net.minecraft.world.level.LevelAccessor;
+// import net.minecraft.world.level.LightLayer;
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(BatEntity.class)
+@Mixin(Bat.class)
 public class BatEntityMixin
 {
-	// private static final Logger LOGGER = LoggerFactory.getLogger("nocturnal-bats");
+	// static private final Logger LOGGER = LoggerFactory.getLogger("nocturnal-bats");
+	// static private long nextLog = 0;
 
-	@Redirect( method="canSpawn", at=@At(value="INVOKE", ordinal=0, target="net/minecraft/util/math/BlockPos.getY()I") )
-	static private int	SpawnAtAllAltitudes(BlockPos spawnPos) {
+	@Redirect(
+		method = "checkBatSpawnRules",
+		at = @At(
+			value = "INVOKE",
+			ordinal = 0,
+			target = "net/minecraft/core/BlockPos.getY()I"
+		)
+	)
+	static private int SpawnAtAllAltitudes(BlockPos spawnPos) {
 		return Integer.MIN_VALUE;
 	}
 
@@ -26,14 +35,24 @@ public class BatEntityMixin
 	 * in vanilla. Only skylight is affected  by ambient darkness. Block light
 	 * levels are unaffected.
 	 */
-	@Redirect( method="canSpawn", at=@At(value="INVOKE", target="net/minecraft/world/WorldAccess.getLightLevel (Lnet/minecraft/util/math/BlockPos;)I") )
-	static private int	FearNotSkylight(WorldAccess world, BlockPos pos) {
-		int darkness = world.getAmbientDarkness() * 12/6;
-		int i = world.getLightLevel(pos, darkness);
+	@Redirect(
+		method = "checkBatSpawnRules",
+		at = @At(
+			value = "INVOKE",
+			target = "net/minecraft/world/level/LevelAccessor.getMaxLocalRawBrightness(Lnet/minecraft/core/BlockPos;)I"
+		)
+	)
+	static private int FearNotSkylight(LevelAccessor world, BlockPos pos) {
+		int darkness = world.getSkyDarken() * 12/6;
+		int i = world.getMaxLocalRawBrightness(pos, darkness);
 
-		// int Skylight = world.getLightLevel(LightType.SKY, pos);
-		// if (Skylight > 0)
-		// 	LOGGER.info(String.format("SKY:%d - DRK:%d = %d", Skylight, darkness, i));
+		// int skylight = world.getBrightness(LightLayer.SKY, pos);
+		// if (skylight > 0 && nextLog < world.getGameTime()){
+		// 	var blockState = world.getBlockState(pos.below());
+		// 	LOGGER.info(String.format("SKY:%d - DRK:%d = %d", skylight, darkness, i));
+		// 	LOGGER.info("Block is Spawnable: {} ({})", blockState.is(BlockTags.BATS_SPAWNABLE_ON), blockState);
+		// 	nextLog = world.getGameTime() + 20;
+		// }
 		
 		return i;
 	}
